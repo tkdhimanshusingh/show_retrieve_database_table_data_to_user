62d535e524ca5594e62fcf4d168aa26246c74ca0
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-23 15:30:42 +0530

    server started
    Show or Retrieve Database Table Data to User

--- enroll/views.py
+++ enroll/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from enroll.models import Student
 # Create your views here.
+def studentinfo(request):
+    stud=Student.objects.all()
+    return render(request,'enroll/studetails.html',{'stu':stud})
