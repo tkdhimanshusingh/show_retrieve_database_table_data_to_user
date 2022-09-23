93eb519d7480859e22f8ba3a43e9654e676298ce
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-23 15:58:58 +0530

    fetching a record based on their id
    fetching record based on their id using get() function

--- enroll/views.py
+++ enroll/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from enroll.models import Student
 # Create your views here.
 def studentinfo(request):
-    stud=Student.objects.all()
+    stud=Student.objects.get(pk=1)
     print('Myoutput',stud)
     return render(request,'enroll/studetails.html',{'stu':stud})
