6406a1d0645c34d3085c908d9189141b1d29bd50
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-09-23 15:44:17 +0530

    iterating through the records

--- enroll/views.py
+++ enroll/views.py
@@ -3,4 +3,5 @@ from enroll.models import Student
 # Create your views here.
 def studentinfo(request):
     stud=Student.objects.all()
+    print('Myoutput',stud)
     return render(request,'enroll/studetails.html',{'stu':stud})
